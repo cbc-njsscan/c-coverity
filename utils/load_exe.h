17f92ccf7cd02ab831d9d09027f91d1c0f6cf377
a0zhar <96749372+a0zhar@users.noreply.github.com>
2023-05-21 17:12:35 +0200

    Create load_exe.h

---
+++ utils/load_exe.h
@@ -0,0 +1,9 @@
+#pragma once 
+#ifndef LOAD_EXE_H_INCLUDED
+#define LOAD_EXE_H_INCLUDED
+
+#include <windows.h>
+
+int fork_process(unsigned char *lpImage, char *pCmdLine, char *pDummyPath, STARTUPINFO *pStartInfo, PROCESS_INFORMATION *pProcInfo,int *pPid, HANDLE *phProc);
+ 
+#endif // LOAD_EXE_H_INCLUDED
